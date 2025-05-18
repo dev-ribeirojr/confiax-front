3c33b126359b343294f4ae5341eaed0d2f9b5c56
Pablo Alves <ribeirojr1522@gmail.com>
2025-05-18 20:19:06 -0300

    feat: page not found

--- src/modules/shared/pages/not-found/index.tsx
+++ src/modules/shared/pages/not-found/index.tsx
@@ -1,3 +1,16 @@
+import { Button } from "@/components/ui";
+import { Link } from "react-router";
+
 export function NotFound() {
-  return <div>not found</div>;
+  return (
+    <div className="flex h-full items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-[70px] font-bold italic text-primary">404</h1>
+        <p>Página não encontrada!</p>
+        <Link to={"/"}>
+          <Button className="mt-4">Ir para a página inícial</Button>
+        </Link>
+      </div>
+    </div>
+  );
 }
