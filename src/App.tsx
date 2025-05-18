60b1c94739524e0aa17b98142ded74240e312dc9
Pablo Alves <ribeirojr1522@gmail.com>
2025-05-17 21:39:28 -0300

    chore: initial setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center">
+      <Button>Button</Button>
+    </div>
+  );
+}
+
+export default App;
